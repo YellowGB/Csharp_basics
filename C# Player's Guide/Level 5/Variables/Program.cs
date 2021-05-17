04ad9c142862fa9dd0a229631ca864882b2f83e3
YellowGB <yellowchickencorp@gmail.com>
2021-05-17 15:26:46 +0200

    Level 5

---
+++ C# Player's Guide/Level 5/Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// PETITS TRUCS CONCERNANT LES VARIABLES EN C#
+
+// Ce qui suit
+int x;
+x = 10;
+
+// Est la même chose que ce qui suit
+int y = 10;
+
+// On peut déclarer plusieurs variables du même type à la fois
+int a, b, c;
+
+// Et on peut assigner la même valeur à plusieurs variables du même type de la façon suivante
+a = b = c = 10;
+
+Console.WriteLine("x = " + x);
+Console.WriteLine("y = " + y);
+Console.WriteLine("a = " + a);
+Console.WriteLine("b = " + b);
+Console.WriteLine("c = " + c);
