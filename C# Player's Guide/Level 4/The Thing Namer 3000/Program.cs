91e183322f464cd0cccdbe685c47bb099a0c4324
YellowGB <yellowchickencorp@gmail.com>
2021-05-17 14:53:56 +0200

    The Thing Namer 3000

---
+++ C# Player's Guide/Level 4/The Thing Namer 3000/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.WriteLine("What kind of thing are taking about?");
+// Name of the thing
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+/*  Description
+    of the thing */
+string b = Console.ReadLine();
+/* Add "Doom" to the name of the thing */
+string c = "Doom";
+string d = "3000"; // Add "3000" to the name of the thing
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
