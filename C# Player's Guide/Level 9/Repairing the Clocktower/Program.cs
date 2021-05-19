54839e15f9d6e2411c4d40595025d3c6e83d8ddd
YellowGB <yellowchickencorp@gmail.com>
2021-05-19 15:54:42 +0200

    Level 9

---
+++ C# Player's Guide/Level 9/Repairing the Clocktower/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.WriteLine("Enter a number");
+int numberClock = Convert.ToInt32(Console.ReadLine());
+
+if (numberClock % 2 != 0)
+    Console.WriteLine("Tick");
+else
+    Console.WriteLine("Tock");
---
+++ C# Player's Guide/Level 9/Watchtower/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Console.Write("Enter the x coordinate of the enemy : ");
+int xCoordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter the y coordinate of the enemy : ");
+int yCoordinate = Convert.ToInt32(Console.ReadLine());
+
+string message = xCoordinate == 0 && yCoordinate == 0 ? "The enemy is here" : "The enemy is to the ";
+string xDirection = "";
+
+if (xCoordinate < 0) xDirection = "west";
+else if (xCoordinate > 0) { xDirection = "east"; }
+
+if (yCoordinate > 0)
+    message += "north";
+else if (yCoordinate < 0)
+{
+    message += "south";
+}
+
+message += xDirection + "!";
+Console.WriteLine(message);
