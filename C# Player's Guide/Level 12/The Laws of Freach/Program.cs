385063f39867a69bfa131d322dd5ecbf901f1521
YellowGB <yellowchickencorp@gmail.com>
2021-05-20 15:25:01 +0200

    Level 12

---
+++ C# Player's Guide/Level 12/The Laws of Freach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentMinimum = int.MaxValue;
+foreach (int index in array)
+    if (index < currentMinimum)
+        currentMinimum = index;
+
+Console.WriteLine(currentMinimum);
+
+int total = 0;
+foreach (int index in array)
+    total += index;
+
+float average = (float)total / array.Length;
+Console.WriteLine(average);
+
+/* Code page 90
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+
+int currentMinimum = int.MaxValue; // Start higher than anything in the array
+for (int index = 0; index < array.Length; index++)
+{
+    if (array[index] < currentMinimum)
+        currentMinimum = array[index];
+}
+
+Console.WriteLine(currentMinimum);
+
+int total = 0;
+for (int index = 0; index < array.Length; index++)
+    total += array[index];
+
+float average = (float)total / array.Length;
+Console.WriteLine(average);
+*/
---
+++ C# Player's Guide/Level 12/The Replicator of D'To/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+int[] array1 = new int[5];
+int index;
+
+for (index = 0; index < array1.Length; index++)
+{
+    Console.Write($"Please enter number {index + 1}: ");
+    array1[index] = Convert.ToInt32(Console.ReadLine());
+}
+
+int[] array2 = new int[array1.Length];
+index = 0;
+
+while (index < array2.Length)
+{
+    array2[index] = array1[index];
+    index++;
+}
+
+index = 0;
+do
+{
+    Console.BackgroundColor = ConsoleColor.White;
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine($"Number {index + 1} in array1: {array1[index]}");
+
+    Console.BackgroundColor = ConsoleColor.Black;
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine($"Number {index + 1} in array2: {array2[index]}");
+    index++;
+}
+while (index < array1.Length);
