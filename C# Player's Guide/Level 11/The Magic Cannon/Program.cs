84ada36e1626135b274b77b556e9fadb44838742
YellowGB <yellowchickencorp@gmail.com>
2021-05-20 13:52:27 +0200

    Level 11

---
+++ C# Player's Guide/Level 11/The Magic Cannon/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+string blastType;
+int fireTurn, electricTurn;
+
+for (int i = 1; i <= 100; i++)
+{
+    fireTurn = i % 3;
+    electricTurn = i % 5;
+    if (fireTurn == 0 && electricTurn == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        blastType = "Electric and Fire";
+    }
+    else if (fireTurn == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        blastType = "Fire";
+    }
+    else if (electricTurn == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        blastType = "Electric";
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        blastType = "Normal";
+    }
+
+    Console.WriteLine($"{i}: {blastType}");
+}
---
+++ C# Player's Guide/Level 11/The Prototype/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+Random rnd = new Random();
+int airshipLocation = rnd.Next(100); // random int entre 0 et 100
+
+int guess;
+Console.WriteLine("Guess the airship location (integer between 0 and 100).");
+
+// do while pour éviter d'avoir à assigner une valeur bidon à guess
+// et laisser le premier input de l'utilisateur s'en charger
+do
+{
+    Console.Write("What is your next guess? ");
+    guess = Convert.ToInt32(Console.ReadLine());
+
+    if (guess < airshipLocation) Console.WriteLine(guess + " is too low.");
+    else if (guess > airshipLocation) Console.WriteLine($"{guess} is too high.");
+}
+while (guess != airshipLocation);
+
+Console.WriteLine("You found the airship!");
