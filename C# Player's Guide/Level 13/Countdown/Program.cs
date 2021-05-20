534b001b0e2e8ca073f2b64b3309aa5c6f871264
YellowGB <yellowchickencorp@gmail.com>
2021-05-21 00:26:17 +0200

    Level 13

---
+++ C# Player's Guide/Level 13/Countdown/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Countdown(10);
+
+/* Original loop
+for (int x = 10; x > 0; x--)
+    Console.WriteLine(x);
+*/
+
+void Countdown(int fromNumber)
+{
+    if (fromNumber == 0) return;
+    Console.WriteLine(fromNumber);
+    Countdown(fromNumber - 1);
+}
---
+++ C# Player's Guide/Level 13/Taking a Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+int result = AskForNumber("What is the airspeed veloccity of an unladen swallow?");
+
+result = AskForNumberInRange("Give a number between 6 and 12.",6,12);
+
+int AskForNumber(string text)
+{
+    Console.Write(text + " ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    return number;
+}
+
+/// <summary>
+/// Display the text parameter and return only if the entered number is between the min and max. Otherwise, ask again.
+/// </summary>
+/// <param name="text">The question to display.</param>
+/// <param name="min">The minimum expected.</param>
+/// <param name="max">The maximum expected.</param>
+int AskForNumberInRange(string text, int min, int max)
+{
+    int number;
+    do
+    {
+        Console.Write(text + " ");
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    while (number < min || number > max);
+
+    return number;
+}
