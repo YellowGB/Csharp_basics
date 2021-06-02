cc977cee1d232b2abab48435a8311832e68ed563
YellowGB <yellowchickencorp@gmail.com>
2021-06-02 16:33:20 +0200

    Exos Xavier 02/06

---
+++ Cours de Xavier/Exercice1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCICE 1
+            /*
+                Créer un tableau contenant les chiffres de 1 à 10 et un autre tableau contenant les nombres de 11 à 20.
+                Ensuite, créer un autre tableau contenant la somme des deux premiers tableaux et afficher ses valeurs.
+                Il faut utiliser les boucles pour créer ces tableaux.
+             */
+            int[] tableau1 = new int[10];
+            int[] tableau2 = new int[10];
+            int[] tableau3 = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                tableau1[i] = i + 1;
+                Console.Write(tableau1[i] + "|");
+
+                tableau2[i] = i + 11;
+                Console.Write(tableau2[i] +"|");
+
+                tableau3[i] = tableau1[i] + tableau2[i];
+                Console.WriteLine(tableau3[i]);
+            }
+
+            // EXERCICE 2
+            /*
+                Voici deux tableaux:
+                - le tableau1 est composé des éléments 6,25,35 et 61
+                - le tableau2 est composé des éléments 12,24 et 46
+                Ecrire le code (boucles) permettant de calculer une valeur représentative
+                de ces deux tableaux notée S. La valeur se calcule en multiplinat chaque valeur
+                du tableau1 par celle du tableau2 et en additionnant le tout.
+             */
+            int[] tableau4 = new int[] { 6, 25, 35, 61 };
+            int[] tableau5 = new int[] { 12,24,46 };
+            int S, ii, j;
+            S = ii = j = 0;
+
+            while (ii < tableau4.Length)
+            {
+                while(j < tableau5.Length)
+                {
+                    S += tableau4[ii] * tableau5[j];
+                    j++;
+                }
+                j = 0;
+                ii++;
+            }
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"La valeur S calculée est : {S}");
+
+            // Methode alternative
+            S = 0;
+            foreach (int i1 in tableau4)
+                foreach (int i2 in tableau5)
+                    S +=i1 * i2;
+
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"La valeur S calculée est : {S}");
+
+            // Methode avec un SUM (aucun intérêt, juste pour le challenge)
+            S = 0;
+            int i5 = 0;
+            int[] tableau6 = new int[tableau4.Length*tableau5.Length];
+            foreach (int i3 in tableau4)
+                foreach (int i4 in tableau5)
+                {
+                    tableau6[i5] = i3 * i4;
+                    i5++;
+                }
+
+            int resultat = tableau6.Sum();
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"La valeur S calculée est : {resultat}");
+
+        }
+    }
+}
--- Cours de Xavier/PremierProjet/Program.cs
+++ Cours de Xavier/PremierProjet/Program.cs
@@ -7,6 +7,78 @@ namespace PremierProjet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // opérateur ternaire
+            //<(expression booleene)>?<retour si true>:<retour si false>;
+
+            // pattern
+            object x = 6;
+            // Motifs constants
+            if (x is 4) Console.WriteLine("x is 4");
+            if (x is 6) Console.WriteLine("x is 6");
+            // Motifs de type
+            if (x is string s) Console.WriteLine("x is string s");
+            if (x is int i) Console.WriteLine("x is int i");
+            // Motifs d'assignation
+            if (x is var v) Console.WriteLine("x is var v");
+
+            // Fonction
+            /*<modificateur d'accès><type retour><NomFonction>([paramètre1],[paramètre2],...)
+             * si on met une valeur par défaut à un paramètre, elle devient facultative lors de l'appel
+             * par exemple [paramètre2=0.2], par défaut la méthode utilisera cette valeur, et elle changera uniquement
+             * si l'utilisateur rentre quelque chose dans ce paramètre
+                {
+                    traitements
+                    return <valeur>;
+                }
+             */
+            Console.WriteLine("Le capitaine a " + AgeDuCapitaine());
+
+            // Classe random
+            Random aleatoire = new Random();
+            int monTirage = aleatoire.Next(1, 30);
+            Console.WriteLine(monTirage);
+
+            // Fonctions de chaînes de caractères (string, char)
+            string maChaine = "Bonjour les gars.";
+            // longueur de la chaîne
+            int longueurChaine = maChaine.Length;
+            Console.WriteLine("La longueur de la chaîne est : " + longueurChaine);
+            // extraction dans la chaîne
+            string extrait1 = maChaine.Substring(12);
+            string extrait2 = maChaine.Substring(0, 4);
+            Console.WriteLine(extrait1);
+            Console.WriteLine(extrait2);
+            // recherche d'index de chaîne
+            int monIndex1 = maChaine.IndexOf("gars");
+            int monIndex2 = maChaine.LastIndexOf("r");
+            Console.WriteLine(monIndex1);
+            Console.WriteLine(monIndex2);
+            Console.WriteLine(maChaine.ToUpper());
+            Console.WriteLine(maChaine.ToLower());
+            // enlever des caractères
+            maChaine = "              Salut les GARS              ";
+            Console.WriteLine(maChaine);
+            Console.WriteLine(maChaine.Trim());
+            maChaine = "Salut les gars!!!!!!!!!";
+            Console.WriteLine(maChaine);
+            Console.WriteLine(maChaine.TrimEnd('!'));
+            maChaine = "Salut";
+            Console.WriteLine(maChaine.PadRight(20, 'x'));
+            string maChaine2 = " à tous";
+            Console.WriteLine(maChaine + maChaine2);
+            // vérifier un contenu dans la chaîne
+            if (maChaine.Contains("al"))
+                Console.WriteLine("oui");
+            else Console.WriteLine("non");
+            // transformation d'une chaîne en tableau sur séparateur
+            string maListe = "Eric,Thierry,Coralie,Paul,Chantale";
+            string[] tabPrenom = maListe.Split(',');
+            Console.WriteLine(tabPrenom[3]);
+        }
+        public static int AgeDuCapitaine()
+        {
+            int ageCapitaine = 25;
+            return ageCapitaine;
         }
     }
 }
