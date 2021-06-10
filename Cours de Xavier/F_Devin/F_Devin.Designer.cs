5c89a6a5ff82fa20cacb74eca272a78a7b7e4ded
YellowChickenCorp <fdaraf@gmail.com>
2021-06-10 18:26:42 +0200

    Forms Xavier 10/06 aprem

---
+++ Cours de Xavier/F_Devin/F_Devin.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace F_Devin
+{
+    partial class F_Devin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblSaisir = new System.Windows.Forms.Label();
+            this.TxtSaisie = new System.Windows.Forms.TextBox();
+            this.CmdOk = new System.Windows.Forms.Button();
+            this.CmdInit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.CmdFin = new System.Windows.Forms.Button();
+            this.LblRandom = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblSaisir
+            // 
+            this.LblSaisir.AutoSize = true;
+            this.LblSaisir.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblSaisir.Location = new System.Drawing.Point(27, 19);
+            this.LblSaisir.Name = "LblSaisir";
+            this.LblSaisir.Size = new System.Drawing.Size(413, 23);
+            this.LblSaisir.TabIndex = 0;
+            this.LblSaisir.Text = "Saisir un nombre compris entre 1 et 9999";
+            // 
+            // TxtSaisie
+            // 
+            this.TxtSaisie.Location = new System.Drawing.Point(454, 20);
+            this.TxtSaisie.MaxLength = 4;
+            this.TxtSaisie.Name = "TxtSaisie";
+            this.TxtSaisie.Size = new System.Drawing.Size(108, 22);
+            this.TxtSaisie.TabIndex = 1;
+            // 
+            // CmdOk
+            // 
+            this.CmdOk.Location = new System.Drawing.Point(582, 19);
+            this.CmdOk.Name = "CmdOk";
+            this.CmdOk.Size = new System.Drawing.Size(52, 23);
+            this.CmdOk.TabIndex = 2;
+            this.CmdOk.Text = "Ok";
+            this.CmdOk.UseVisualStyleBackColor = true;
+            this.CmdOk.Click += new System.EventHandler(this.CmdOk_Click);
+            // 
+            // CmdInit
+            // 
+            this.CmdInit.Enabled = false;
+            this.CmdInit.Location = new System.Drawing.Point(660, 19);
+            this.CmdInit.Name = "CmdInit";
+            this.CmdInit.Size = new System.Drawing.Size(115, 23);
+            this.CmdInit.TabIndex = 3;
+            this.CmdInit.Text = "Retirage";
+            this.CmdInit.UseVisualStyleBackColor = true;
+            this.CmdInit.Click += new System.EventHandler(this.CmdInit_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(53, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 25);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(53, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 25);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(53, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(287, 25);
+            this.label3.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(53, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(287, 25);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(53, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(287, 25);
+            this.label5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(53, 304);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(287, 25);
+            this.label6.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(454, 74);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(287, 25);
+            this.label7.TabIndex = 15;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(454, 120);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(287, 25);
+            this.label8.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label9.Location = new System.Drawing.Point(454, 166);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(287, 25);
+            this.label9.TabIndex = 13;
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label10.Location = new System.Drawing.Point(454, 212);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(287, 25);
+            this.label10.TabIndex = 12;
+            // 
+            // label11
+            // 
+            this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label11.Location = new System.Drawing.Point(454, 258);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(287, 25);
+            this.label11.TabIndex = 11;
+            // 
+            // label12
+            // 
+            this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label12.Location = new System.Drawing.Point(454, 304);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(287, 25);
+            this.label12.TabIndex = 10;
+            // 
+            // CmdFin
+            // 
+            this.CmdFin.Location = new System.Drawing.Point(285, 362);
+            this.CmdFin.Name = "CmdFin";
+            this.CmdFin.Size = new System.Drawing.Size(195, 42);
+            this.CmdFin.TabIndex = 16;
+            this.CmdFin.Text = "Terminer";
+            this.CmdFin.UseVisualStyleBackColor = true;
+            this.CmdFin.Click += new System.EventHandler(this.CmdFin_Click);
+            // 
+            // LblRandom
+            // 
+            this.LblRandom.AutoSize = true;
+            this.LblRandom.Location = new System.Drawing.Point(114, 375);
+            this.LblRandom.Name = "LblRandom";
+            this.LblRandom.Size = new System.Drawing.Size(54, 17);
+            this.LblRandom.TabIndex = 17;
+            this.LblRandom.Text = "label13";
+            this.LblRandom.Visible = false;
+            // 
+            // F_Devin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblRandom);
+            this.Controls.Add(this.CmdFin);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CmdInit);
+            this.Controls.Add(this.CmdOk);
+            this.Controls.Add(this.TxtSaisie);
+            this.Controls.Add(this.LblSaisir);
+            this.Name = "F_Devin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Devinez un nombre";
+            this.Load += new System.EventHandler(this.F_Devin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblSaisir;
+        private System.Windows.Forms.TextBox TxtSaisie;
+        private System.Windows.Forms.Button CmdOk;
+        private System.Windows.Forms.Button CmdInit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button CmdFin;
+        private System.Windows.Forms.Label LblRandom;
+    }
+}
+
---
+++ Cours de Xavier/F_Devin/F_Devin.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F_Devin
+{
+    public partial class F_Devin : Form
+    {
+        public int monTirage;
+        public int nbTour = 0;
+        public F_Devin()
+        {
+            InitializeComponent();
+        }
+
+        private void F_Devin_Load(object sender, EventArgs e)
+        {
+            monTirage = RandomNumber(1, 9999);
+            LblRandom.Text = monTirage.ToString();
+        }
+
+        static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        private void CmdFin_Click(object sender, EventArgs e)
+        {
+            Form.ActiveForm.Close();
+        }
+
+        string Resultat(string monNombre)
+        {
+            int monNombre2 = int.Parse(monNombre);
+            string maChaine = "";
+
+            if(monNombre2 < monTirage)
+            {
+                maChaine = monNombre + " est inférieur au tirage";
+            } else if(monNombre2 > monTirage)
+            {
+                maChaine = monNombre + " est supérieur au tirage";
+            } else
+            {
+                maChaine = "Gagné, vous avez trouvé !";
+                // il n'est plus nécessaire de cliquer sur ok puisque c'est gagné
+                CmdOk.Enabled = false;
+                // possibilité de rejouer
+                CmdInit.Enabled = true;
+            }
+            return maChaine;
+        }
+        private void CmdOk_Click(object sender, EventArgs e)
+        {
+            // pour le TryParse
+            int n;
+            // Contrôle de saisie
+            if(TxtSaisie.Text == "")
+            {
+                MessageBox.Show("Vous devez saisir un nombre entre 1 et 9999 !");
+                TxtSaisie.Focus();
+                return;
+            }
+            else if (!Int32.TryParse(TxtSaisie.Text, out n))
+            {
+                MessageBox.Show("Vous devez saisir un nombre entre 1 et 9999 !");
+                TxtSaisie.Text = "";
+                TxtSaisie.Focus();
+                return;
+            }
+            // pas besoin du OR car la propriété MaxLength de TxtSaisie est de 4 chiffres
+            else if (Convert.ToInt32(TxtSaisie.Text) < 1 /*|| Convert.ToInt32(TxtSaisie.Text) > 9999*/)
+            {
+                MessageBox.Show("Vous devez saisir un nombre entre 1 et 9999 !");
+                TxtSaisie.Text = "";
+                TxtSaisie.Focus();
+                return;
+            }
+
+            // pas la meilleure solution pour comparer des nombres en string
+            // car en comparaison de string 1120 est inférieur à 120 par exemple
+            // (11 est inférieur à 12)
+            //label1.Text = TxtSaisie.Text == LblRandom.Text ? "Gagné !" : "Perdu";
+
+            nbTour++;
+            switch (nbTour)
+            {
+                case 1:
+                    label1.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 2:
+                    label2.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 3:
+                    label3.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 4:
+                    label4.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 5:
+                    label5.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 6:
+                    label6.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 7:
+                    label7.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 8:
+                    label8.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 9:
+                    label9.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 10:
+                    label10.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 11:
+                    label11.Text = Resultat(TxtSaisie.Text);
+                    break;
+                case 12:
+                    label12.Text = Resultat(TxtSaisie.Text);
+                    if (label12.Text != "Gagné, vous avez trouvé !")
+                        label12.Text = $"Perdu, c'était {monTirage}, utilise la dychotomie !";
+                    break;
+            }
+            TxtSaisie.Text = "";
+        }
+
+        private void CmdInit_Click(object sender, EventArgs e)
+        {
+            monTirage = RandomNumber(1, 9999);
+            LblRandom.Text = monTirage.ToString();
+
+            CmdInit.Enabled = false;
+            CmdOk.Enabled = true;
+
+            nbTour = 0;
+            label1.Text = "";
+            label2.Text = "";
+            label3.Text = "";
+            label4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+            label7.Text = "";
+            label8.Text = "";
+            label9.Text = "";
+            label10.Text = "";
+            label11.Text = "";
+            label12.Text = "";
+        }
+    }
+}
